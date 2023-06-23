79c5c2686d2d0bbd7d35d8491e1c43d00f2d9513
Jandueza <jonathanandueza1201@gmail.com>
2023-06-23 15:36:33 -0300

    Basic styles & routes

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 import {  default as Navbar } from './Navbar';
+import {  default as Exchanges } from './Exchanges.js';
+import {  default as Homepage } from './Homepage';
+import {  default as Cryptocurrencies } from './Cryptocurrencies.js';
+import {  default as News } from './News.js';
+import {  default as CryptoDetails } from './CryptoDetails.js';
