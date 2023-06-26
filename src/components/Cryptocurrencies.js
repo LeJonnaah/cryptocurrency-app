ae9bd1969a7b6041c2a2c2a2d67f00f96a9f87db
Jandueza <jonathanandueza1201@gmail.com>
2023-06-26 16:01:55 -0300

    Loader spinner integration

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 
 import { useGetCryptosQuery } from '../services/cryptoAPI'
+import Loader from './Loader'
 
 export default function Cryptocurrencies({ simplified }) {
 
@@ -20,7 +21,7 @@ export default function Cryptocurrencies({ simplified }) {
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
-  if (isFetching) return 'Loading...';
+  if (isFetching) return <Loader />;
 
   return (
     <>
