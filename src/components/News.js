cd0217d3e303cb2d167c5db83b66ab74e4499abb
Jandueza <jonathanandueza1201@gmail.com>
2023-06-28 16:03:55 -0300

    News fixes

--- src/components/News.js
+++ src/components/News.js
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Text, Title } = Typography
 const { Option } = Select
 
 export default function News({ simplified }) {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 })
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 })
+  const { data } = useGetCryptosQuery(100)
 
-  console.log(cryptoNews)
 
   if (!cryptoNews?.value) return 'Loading...'
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+          </Select>
+        </Col>
+      )}
+
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
             <Link to={`/news/${news.url}`}
               target="_blank"
+              rel='noreferrer'
             >
               <div className="news-image-container">
                 <Title className="news-title" level={4}>{news.name}</Title>
