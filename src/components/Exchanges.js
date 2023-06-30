fc1d04b938e8656f637894384bc3651ba6ac3a05
Jandueza <jonathanandueza1201@gmail.com>
2023-06-30 16:35:28 -0300

    exchanges init

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Col, Row, Typography } from 'antd'
+import millify from 'millify'
+import HTMLReactParser from 'html-react-parser'
+import { useGetExchangesQuery } from '../services/cryptoApi'
+import Loader from './Loader'
 
-export default function Exchanges() {
-  return (
-    <div>Exchanges</div>
-  )
+const { Text } = Typography
+
+const Exchanges = () => {
+    const { data, isFetching } = useGetExchangesQuery()
+    const exchangesList = data?.data?.exchanges
+
+    if (isFetching) return <Loader />
+
+    return (
+        <>
+            <Row>
+                <Col span={6}>Exchanges</Col>
+                <Col span={6}>24h Trade Volume</Col>
+                <Col span={6}>Markets</Col>
+                <Col span={6}>Change</Col>
+            </Row>
+            {exchangesList.map((exchange) => (
+                <Row>
+                    <Col span={6}>
+                        <Text><strong>{exchange.rank}.</strong></Text>
+                        <Text><strong>{exchange.name}</strong></Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                </Row>
+            ))}
+        </>
+    )
 }
+
+export default Exchanges
