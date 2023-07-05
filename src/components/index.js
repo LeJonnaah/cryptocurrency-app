b9a23f56c2d67f9f82166cfa4ced74f95b48d9a5
Jandueza <jonathanandueza1201@gmail.com>
2023-07-05 17:05:24 -0300

    index fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import {  default as Navbar } from './Navbar';
-import {  default as Exchanges } from './Exchanges.js';
 import {  default as Homepage } from './Homepage';
 import {  default as Cryptocurrencies } from './Cryptocurrencies.js';
 import {  default as News } from './News.js';
@@ -7,7 +6,6 @@ import {  default as CryptoDetails } from './CryptoDetails.js';
 
 export {
     Navbar,
-    Exchanges,
     Homepage,
     Cryptocurrencies,
     News,
