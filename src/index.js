d8c87745da6828d5c94e8a2087c8ea8e70dfe085
Jandueza <jonathanandueza1201@gmail.com>
2023-07-06 16:06:17 -0300

    store path changed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App.js';
-import store from './app/store.js';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
     <Router>
